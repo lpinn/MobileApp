1c097718120341fb73db69c4fac8b0904193bc56
Alex Wang <alw245@pitt.edu>
2021-02-07 12:36:10 -0500

    dummy image

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import beans from "./assets/beans.jpg";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text styles = {styles.header}>Building New Hope</Text>
+      <Image source={beans} style={styles.logo} />
+      <Text style={styles.header}>Building New Hope</Text>
+      <TouchableOpacity />
       <StatusBar style="auto" />
-      <Text>Hello World</Text>
     </View>
   );
 }
@@ -15,12 +17,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   header: {
-    fontSize: 80,
-    color: '#fff',
-  }
+    fontSize: 25,
+    padding: 20,
+    color: "#fff",
+  },
+  logo: {
+    width: 256,
+    height: 256,
+    marginBottom: 20,
+  },
 });
