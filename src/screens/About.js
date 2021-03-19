84ce1c1c9fb956664a249087d840686462065d4a
Alex Wang <alw245@pitt.edu>
2021-03-19 16:51:35 -0400

    feat: inital splash screen - unstyled

---
+++ src/screens/About.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+import { Text } from "react-native-elements";
+
+export default function About() {
+  return (
+    <>
+      <Text h3> Building New Hope</Text>
+      <Text>
+        Building New Hope is a volunteer-driven nonprofit organization based in
+        Pittsburgh and Nicaragua. In Nicaragua, we partner with a worker-owned
+        organic coffee cooperative, operate two supplemental schools for at-risk
+        youth, and provide high school scholarships for students in need. In
+        Pittsburgh, we engage with local schools and universities, faith groups,
+        farmers, non-profits, community groups, and businesses to promote
+        healthy ecosystems and to spread awareness about how fair trade
+        relationships can help to reduce poverty and inequalities. Our reach may
+        be broad but our mission is simple: help those in need by providing
+        educational opportunities and fair prices for goods and services
+      </Text>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
